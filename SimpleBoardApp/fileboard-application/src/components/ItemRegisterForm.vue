ff00e499c6bd1334b85708fbc86ecf04ea5d0723
DESKTOP-33NPRVQ\Dell <husuh@diquest.com>
2020-11-20 16:46:53 +0900

    update fileupload

--- SimpleBoardApp/fileboard-application/src/components/ItemRegisterForm.vue
+++ SimpleBoardApp/fileboard-application/src/components/ItemRegisterForm.vue
@@ -1,13 +1,50 @@
 <template>
-    <div>
-        <h3>상품 등록 폼</h3>
-        <button type="submit">등록</button>
-        <router-link :to="{ name: 'ItemListPage' }">취소</router-link>
-    </div>
+    <form @submit.prevent="onSubmit">
+        <table>
+            <tr>
+                <td>상품명</td>
+                <td><input type="text" v-model="itemName"></td>
+            </tr>
+            <tr>
+                <td>상품가격</td>
+                <td><input type="text" v-model="price">&nbsp;원</td>
+            </tr>
+            <tr>
+                <td>상품파일</td>
+                <td><input type="file" @change="handlerFileChange($event)"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>상품설명</td>
+                <td><textarea v-model="description" row="5"></textarea></td>
+            </tr>
+        </table>
+        <div>
+            <button type="submit">등록</button>
+            <router-link :to="{ name: 'ItemListPage' }">취소</router-link>
+        </div>
+    </form>
 </template>
 
 <script>
 export default {
-    name: 'ItemRegisterForm'
+    name: 'ItemRegisterForm',
+    data: {
+        itemName: '',
+        price: '',
+        description: '',
+        file: ''
+    },
+    methods: {
+        onSubmit(){
+            const { itemName, price, description, file } = this
+            this.$emit('submit',{ itemName, price, description, file })
+        },
+        // 파일을 선택하면 실행하는 메소드
+        handleFileChange(evt){
+            console.log(evt.target.files)
+            this.file = evt.target.files[0]
+        }
+    }
 }
 </script>
--- SimpleBoardApp/fileboard-application/src/views/ItemRegisterPage.vue
+++ SimpleBoardApp/fileboard-application/src/views/ItemRegisterPage.vue
@@ -1,17 +1,49 @@
 <template>
     <div align="center">
         <h2>상품 등록</h2>
-        <item-register-form/>
+        <item-register-form @submit="onSubmit"/>
     </div>
 </template>
 
 <script>
 import ItemRegisterForm from '@/components/ItemRegisterForm'
-
-export default{
+import api from '@/api'
+export default {
     name: 'ItemRegisterPage',
     components: {
         ItemRegisterForm
+    },
+    methods: {
+        onSubmit(payload){
+            const { itemName, price, description, file } = payload
+            const itemObject = {
+                itemName :itemName,
+                price: price,
+                description: description
+            }
+
+            let formData = new FormData()
+
+            formData.append("file", file)
+            formData.append("item", JSON.stringify(itemObject))
+
+            api.post('./items', formData,{
+                headers: {
+                    // multipart 형식으로 지정
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then( res => {
+                alert('파일이 정상적으로 등록되었습니다.')
+                this.$router.push({
+                    name: 'ItemReadPage',
+                    params: { itemId: res.data.itemId }
+                })
+            }).catch( err => {
+                console.log(err.response.data.msg)
+                alert('오류 발생으로 파일 업로드에 실패하였습니다.')
+            })
+        }
     }
+
 }
 </script>
