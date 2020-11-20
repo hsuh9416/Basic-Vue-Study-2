0710a288e9210cd85e470a7a86ff304258879898
DESKTOP-33NPRVQ\Dell <husuh@diquest.com>
2020-11-20 18:00:51 +0900

    update modifying

--- SimpleBoardApp/fileboard-application/src/components/ItemModifyForm.vue
+++ SimpleBoardApp/fileboard-application/src/components/ItemModifyForm.vue
@@ -1,13 +1,74 @@
 <template>
-    <div>
-        <h3>상품 수정 폼</h3>
-        <button type="submit">수정</button>
-        <router-link :to="{ name: 'ItemReadPage',params: { itemId: 100 } }">취소</router-link>
-    </div>
+    <form @submit.prevent="onSubmit">
+        <table>
+            <tr>
+                <td>상품번호</td>
+                <td><input :value="item.itemId" type="text" disabled></td>
+            </tr>
+            <tr>
+                <td>상품명</td>
+                <td><input type="text" v-model="itemName"></td>
+            </tr>
+            <tr>
+                <td>상품가격</td>
+                <td><input type="text" v-model="price">&nbsp;원</td>
+            </tr>
+            <tr>
+                <td>상품파일</td>
+                <td><input type="file" @change="handlerFileChange($event)"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>미리보기</td>
+                <td><img v-bind:src="pictureUrl()" width="200"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>상품설명</td>
+                <td><textarea v-model="description" row="5"></textarea></td>
+            </tr>
+        </table>
+        <div>
+            <button type="submit">수정</button>
+            <router-link :to="{ name: 'ItemListPage', params: { itemId: item.itemId.toString() } }">취소</router-link>
+        </div>
+    </form>
 </template>
 
 <script>
 export default {
-    name: 'ItemModifyForm'
+    name: 'ItemModifyForm',
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            itemName: '',
+            price: 0,
+            description: '',
+            file: ''
+        }
+    },
+    methods: {
+        onSubmit(){
+            const {itemName, price, description, file } = this
+            this.$emit('submit',{ itemName, price, description, file } )
+        },
+        handleFileChange(evt){
+            console.log(evt.target.files)
+            this.file = evt.target.files[0]
+        },
+        pictureUrl: function(){
+            return 'http://localhost:8080/items/display?itemId=' + this.item.itemId + '&timestamp=' + new Date().getTime()
+        } 
+    },
+    created(){
+        this.itemName = this.itemitemName
+        this.price = this.item.price
+        this.description = this.item.description
+    }
 }
 </script>
--- SimpleBoardApp/fileboard-application/src/views/ItemModifyPage.vue
+++ SimpleBoardApp/fileboard-application/src/views/ItemModifyPage.vue
@@ -1,7 +1,8 @@
 <template>
     <div align="center">
         <h2>상품 수정</h2>
-        <item-modify-form/>
+        <item-modify-form v-if="item" :item="item" @submit="onSubmit"/>
+        <p v-else>loading...</p>
     </div>
 </template>
 
@@ -9,9 +10,60 @@
 
 import ItemModifyForm from '../components/ItemModifyForm.vue'
 import { mapState, mapActions } from 'vuex'
+import api from '@/api'
 
 export default {
     name: 'ItemModifyPage',
-    components: { ItemModifyForm }
+    components: { ItemModifyForm },
+    props: {
+        itemId: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        ...mapState([ 'item' ])
+    },
+    methods: {
+        ...mapActions(['fetchItem']),
+        onSubmit( payload ){
+            const { itemName, price, description, file } = payload
+            const itemObject = {
+                itemId: this.itemId,
+                itemName :itemName,
+                price: price,
+                description: description
+            }
+
+            let formData = new FormData()
+
+            formData.append("file", file)
+            formData.append("item", JSON.stringify(itemObject))
+
+            api.post('./items', formData,{
+                headers: {
+                    // multipart 형식으로 지정
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then( res => {
+                alert('파일이 정상적으로 수정되었습니다.')
+                this.$router.push({
+                    name: 'ItemReadPage',
+                    params: { itemId: res.data.itemId.toString() }
+                })
+            }).catch( err => {
+                console.log(err.response.data.msg)
+                alert('오류 발생으로 파일 수정에 실패하였습니다.')
+            })
+        }
+    },
+    created(){
+        this.fetchItem(this.itemId)
+        .catch( err => {
+            console.log(err.response.data.msg)
+            alert('오류로 인하여 페이지를 불러오지 못하였습니다.')
+            this.$router.back()
+        })
+    }
 }
 </script>
